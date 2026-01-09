1fa1e7f8a52ffb39aa1dd1f25212ee7618c6f4bc
Mohd Zaid <mdzaid0205@gmail.com>
2026-01-09 11:09:31 +0000

    os clears buffers and shows loading text

--- src/kernel.s
+++ src/kernel.s
@@ -1,12 +1,53 @@
+[ORG 0x1000]
 [BITS 32]
+
+; what to write %1
+; offset to write %2
+%macro print 3
+    
+    jmp %%PREP
+    %%M: db %1
+    %%E: db 0h
+
+%%PREP:
+    mov edi, 0xB8000
+    add edi, %2
+    mov esi, %%M
+    mov ah, %3
+%%LOOP:
+    lodsb
+    ;; load into ah
+    or al, al
+    jz %%DONE
+
+    mov [edi], ax
+    add edi, 2
+
+    jmp %%LOOP
+%%DONE:
+%endmacro
 
 _kernel:
     .start:
         ;; remember start address of kernel main
     
-    mov eax, 0x01
-    mov esi, 0x02
-    mov edi, 0x00
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esp, 0x90000    
+    
+    mov edi, 0xB8000
+    mov ecx, 2000
+
+    mov ax, 0x1F20
+    rep stosw
+
+    mov edi, 0xB8000
+    print "Welcome to ", 1980, 0x1f
+    print "VenOS :-]", 2002, 0b00011111
 
     jmp $
 
