14c2711686c6d8afaec4787ebd07014157d89ccd
Mohd Zaid <mdzaid0205@gmail.com>
2026-02-09 12:47:29 +0000

    BOOT/STAGE2: Implement `print_hex` for e820 implementation

--- boot/stage2.asm
+++ boot/stage2.asm
@@ -41,7 +41,7 @@ print_string:
 
     .loop:
         lodsb               ;; Load byte at DS:SI into AL
-        cmp al, 0           ;; if end string
+        cmp al, 0x00        ;; if end string
         je  .done           ;; completed
         int 0x10            ;; print
         jmp .loop           ;; loop and print next chat
@@ -57,5 +57,38 @@ print_string:
         int 0x10
 
         popa        ;; restore context before retutning to caller
+ret
+
+print_hex:
+    ;;  prints only the value present in dx register (16 bits register)
+    
+    pusha           ;; store context to restore after call returns
+    mov cx, 0x04    ;; required parameter for int 0x10
+
+    .loop:
+        mov ax, dx          ;; Copy the value from DX to AX for manipulation
+        and ax, 0xF000      ;; keep only the top 4 bits (the first hex digit)
+        shr ax, 0x0c        ;; ignore lower 12 bit in order to print first 4
+                            ;; (Example: 0xF000 becomes 0x000F)
+
+        cmp ax, 0x09        ;; Check if it's 0-9 or a-f
+        jg .alpha           ;; If greater than 9, it's a letter
+
+    .numeric:
+        add al, 0x30        ;; Convert 0-9 to ASCII '0'-'9' (0x30)
+        jmp .print          ;; jump and print this value
+
+    .alpha:
+        add al, 0x61        ;; Convert 10-15 to ASCII 'A'-'F'
+        sub al, 0x0a        ;; remove 
+    
+    .print:
+        mov ah, 0x0E        ;; BIOS teletype function
+        int 0x10            ;; Print the character in AL
+
+        shl dx, 0x04        ;; Shift DX left by 4 bits to bring the next digit 
+        loop .loop          ;; Decrement CX and jump to .loop if CX > 0
+
+    popa                ; Restore registers
 ret
 
